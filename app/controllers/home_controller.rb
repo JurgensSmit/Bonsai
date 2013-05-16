a3f668d62ab4a70e06d99ef6864de4030f8e44ba
Jurgens Smit <contact@jurgenssmit.com>
2013-05-16 20:46:48 +1000

    display posts on homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
     @users = User.all
     @posts = Post.all
     @comments = Comment.all
-    @posts = Post.paginate(:page => params[:page], :per_page => 3)
+    @posts = Post.paginate(:page => params[:page], :per_page => 6)
   end
 end
