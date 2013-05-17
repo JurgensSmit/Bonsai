86f9eb6dc51199cf4ae2974faa0d594ca61bb4f9
Jurgens Smit <contact@jurgenssmit.com>
2013-05-17 16:24:58 +1000

    fix comment display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @posts = Post.all
-    @comments = Comment.all
+
     @posts = Post.paginate(:page => params[:page], :per_page => 6)
   end
 end
